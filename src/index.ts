4ac0f84c530c4bfd5bdb7b2393534d0af1aba2be
Ian Cole <ianvernoncole@gmail.com>
2022-03-28 09:45:46 -0600

    Added Bootstrap and AWS resources

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,29 @@
 import express, {Request,Response,Application} from 'express';
-import { generate } from "./worldgen/Generator";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 const app:Application = express();
 var path = require('path');
+app.use(express.static("public"));
 
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAAshHMLxANSiAtdoY9r9vZuyQbeZLI0iY",
+  authDomain: "lifescore-532e4.firebaseapp.com",
+  projectId: "lifescore-532e4",
+  storageBucket: "lifescore-532e4.appspot.com",
+  messagingSenderId: "390123766451",
+  appId: "1:390123766451:web:d5e0462fb6cb03279126c6",
+  measurementId: "G-E93SMMBSRJ"
+};
+
+// Initialize Firebase
+const fb = initializeApp(firebaseConfig);
+const analytics = getAnalytics(fb);
 
 // The code below allows the node js to find the public directory with the index.html file
 const publicPath = path.join(__dirname, '../public');
@@ -12,8 +33,9 @@ app.set('view engine', 'ejs');
 
 /* GET index page */
 app.get("/", (req:Request, res:Response):void => {
-  let out = generate()
-   res.render('index', {output : out});
+  
+   // res.render('index', {output : "e"});
+   res.render('index');
 });
 
 app.listen(port, () => {
